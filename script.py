a1b4249c955d083f4fd3c594b7d97d9e7f1e5dae
laxa <laxa@ddracepro.net>
2016-07-06 21:36:09 +0200

    Should be good

---
+++ script.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import requests.packages.urllib3
+import requests
+import os.path
+import time
+import json
+import re
+
+URL = "https://www.teamspeak.com/downloads"
+FILE = "VERSION"
+
+if __name__ == "__main__":
+    requests.packages.urllib3.disable_warnings()
+    while True:
+        data = requests.get(URL)
+        if data.status_code == 200:
+            break
+        time.sleep(60)
+    client = re.findall("Client [^>]+([^<]+)", data.text)[0][1:]
+    server = re.findall("Server [^>]+([^<]+)", data.text)[0][1:]
+    if os.path.isfile(FILE):
+        with open(FILE, "r") as f:
+            prev = json.loads(f.read())
+        if prev["client"] != client:
+            print "Client is now: " + client
+        if prev["server"] != server:
+            print "Server is now: " + client
+        with open(FILE, "w") as f:
+            f.write(json.dumps({"server":server, "client":client}))
+    else:
+        print "Client: %s\nServer: %s" % (client, server)
+        with open(FILE, "w") as f:
+            f.write(json.dumps({"server":server, "client":client}))
